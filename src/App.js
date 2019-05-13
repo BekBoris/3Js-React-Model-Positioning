39a3cb5c9bb7859be242340843a7f6684fb64ac5
Boris <bekboris@gmail.com>
2019-05-14 00:43:44 +0400

    Transform Control Option

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import './style.css'
 import React from 'react';
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-import fileGlb from './meat.glb'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import fileGlb from './bread.glb' // GLB FILE
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     // STATE FOR UPDATING INPUTS VALUE AND SET MODEL POSITION
-    this.state = {cameraPosition: { x: 0, y: 0, z: 0.5 },
-                  position: { x: 0, y: 0, z: 0 },
-                  rotation: { x: 0.9, y: 0, z: 0 },
-                  scale: { x: 1, y: 1, z: 1 } }
+    this.state = {position: { x: 0, y: 0, z: 0 },
+                  rotation: { x: 0, y: 0, z: 0 },
+                  scale: { x: 3, y: 3, z: 3 } }
   }
-  // FUNCTION FOR SAVE VALUES FORM INPUT AND MOUSE POINTER
+  // FUNCTION FOR SAVE VALUES FORM INPUTS
   stateUpdate = (event, direction, axis, ajustFunc) => {
     // CHANGING EVENT TO MOUSE DATA IF...
     const targetValue = typeof event === "number" ? event : event.target.value;
@@ -27,15 +27,6 @@ class App extends React.Component {
   componentDidMount = () => {
     const width = this.mount.clientWidth;
     const height = this.mount.clientHeight;
-    //ADD SCENE
-    this.scene = new THREE.Scene();
-    //ADD CAMERA
-    this.camera = new THREE.PerspectiveCamera( 75, width / height, 0.1, 1000 );
-    // CAMERA POSITION 
-    this.camera.position.z = 0.5;
-    // LIGHT
-    var light = new THREE.AmbientLight(0xffffff, 3.3);
-    this.scene.add(light);
     //ADD RENDERER
     this.renderer = new THREE.WebGLRenderer({antialias: true});
     this.renderer.setClearColor('#dbdbdb');
@@ -43,100 +34,139 @@ class App extends React.Component {
     this.renderer.gammaFactor = 1.5;
     this.renderer.gammaOutput = true;
     this.mount.appendChild(this.renderer.domElement);
-    // ADDING MOUSE EVENT LISTNER TO GET MOUSE POSITION DATA
-    const addSmt = this.mount.appendChild(this.renderer.domElement);
-    addSmt.addEventListener('mousemove', this.onMouseMove, false);
-    // LODING GLB FILE FROM ROOT FOLDER
-    this.loader = new GLTFLoader();
-    this.loader.load(fileGlb, gltf => {
-      // FIRST MODEL  INITIALIZATION
-      this.scene.add(gltf.scene);
-      gltf.scene.rotation.x = 0.9;
 
+    //ADD CAMERA
+    this.camera = new THREE.PerspectiveCamera( 75, width / height, 0.1, 10000 );
+
+    //ADD SCENE
+    this.scene = new THREE.Scene();
+
+    // LIGHT
+    this.light = new THREE.AmbientLight(0xffffff, 3.3);
+    this.scene.add(this.light);
+
+    // ADD GRID HELPER
+    const size = 10;
+    const divisions = 25;
+    const gridHelper = new THREE.GridHelper( size, divisions );
+    this.scene.add( gridHelper );
+
+
+
+
+    //ADD TRANSFORM CONTROL FROM INDEX.HTML
+    this.control = new window.THREE.TransformControls( this.camera, this.renderer.domElement );
+    this.control.setSize(1);
+
+    // ADD EVENT LISTNER TO MOVE MODEL AND CHANGE REACT STATE TO CHANGE VALUSE IN INPUTS
+    this.control.addEventListener( 'change', () => {
+      this.renderer.render(this.scene, this.camera)
+      this.updateSetState() ;} );
+
+    // EVENT LISTNER TO DISABLE ORBIT MOVE
+    this.control.addEventListener( 'dragging-changed', ( event ) => this.orbit.enabled = ! event.value);
+
+    // ORBIT CONTROL
+    this.orbit = new OrbitControls( this.camera, this.renderer.domElement );
+    this.camera.position.set( 0, 2, 2 );
+    this.orbit.update();
+
+    //EVENT LISTNER TO VIEW MODEL IN DIFFERENT POSITIONS
+    this.orbit.addEventListener("change", () => this.renderer.render(this.scene, this.camera));
+
+    // ADD LOADER FROM NPM FOLDER
+    this.loader = new GLTFLoader();
+
+    // LODING GLB FILE FROM SRC FOLDER
+    this.loader.load(fileGlb, gltf => {
+      this.gltf = gltf.scene
+
+      // ADD MODEL TO THE SCENE
+      this.scene.add(gltf.scene);
+
+      // ATTACH MODEL TO TRANSFORM CONTROL
+      this.control.attach( gltf.scene );
+			this.scene.add( this.control );
+
+      //
+      gltf.scene.scale.set(3, 3, 3)
+      this.orbit.update();
 
       this.renderer.render(this.scene, this.camera);
-    }, undefined, error => {
-      console.error(error);
+    }, undefined,
+
+    error => {
+      console.log(error);
     });
-  };
-  // CAMERA MOVE INITIALIZATION X Y Z AND RENDERING
-  cameraMove = (direction, axis) => {
-    this.camera.position[axis] = this.state[direction][axis];
-    this.renderer.render(this.scene, this.camera);
-  };
-  // POSITION INITIALIZATION X Y Z AND RENDERING
-  objPosition = (direction, axis) => {
-    this.scene[direction][axis] = this.state[direction][axis];
-    this.renderer.render(this.scene, this.camera);
-  };
-  // ROTATION INITIALIZATION X Y Z AND RENDERING
-  objRotation = (direction, axis) => {
-    this.scene[direction][axis] = this.state[direction][axis];
-    this.renderer.render(this.scene, this.camera);
-  };
-  // SCALE INITIALIZATION X Y Z AND RENDERING
-  objScale = (direction, axis) => {
-    this.scene[direction][axis] = this.state[direction][axis];
-    this.renderer.render(this.scene, this.camera);
+
   };
 
-  // GETING DATA FORM MOUSE POSITION AND SENDING TO STATEUPDATE TO SAVE IN STATE
-  onMouseMove = event => {
-    // getiong mouse cordinates from threejs canvas
-    let mouseX = (event.clientX - event.target.getBoundingClientRect().left) / event.target.getBoundingClientRect().width * 2 - 1;
-    let mouseY = (event.clientY - event.target.getBoundingClientRect().top) / event.target.getBoundingClientRect().height * 2 - 1;
-    const xyAxis = this.valueAxis.value === "x" ? Math.round(mouseX * 1000) / 1000 : Math.round(mouseY * 1000) / 1000;
-    if (event.which === 1) {
+  // UPDATE REACT STATE
+  updateSetState = () => {
+    if (this.gltf !== undefined) {
+      this.setState((state) => {
+        const gltf = this.gltf[this.valType.value]
+        return state[this.valType.value] = {x:gltf.x, y:gltf.y, z:gltf.z,}
+      });
+    };
+  }
+
+
+  // GETING DATA FORM SELECT ELEMENT TO SET POSITION SCALE OR ROTATION IN TRANSFORMCONTROL
+  posRotScale = () => {
       switch (this.valType.value) {
-        case "cameraPosition":
-          this.stateUpdate(xyAxis, this.valType.value, this.valueAxis.value, this.cameraMove);
-          break;
 
         case "position":
-          this.stateUpdate(xyAxis, this.valType.value, this.valueAxis.value, this.objPosition);
+          this.control.setMode( "translate" );
           break;
 
         case "rotation":
-          this.stateUpdate(xyAxis * 10, this.valType.value, this.valueAxis.value, this.objRotation);
+          this.control.setMode( "rotate" );
           break;
 
         case "scale":
-          this.stateUpdate(xyAxis, this.valType.value, this.valueAxis.value, this.objScale);
+          this.control.setMode( "scale" );
           break;
 
         default:;
-      }
+
     }
+  };
+
+  // MODEL POSITION CHANGE VIA POSITION INPUT
+  objPosition = (direction, axis) => {
+    this.gltf[direction][axis] = this.state[direction][axis];
+    this.renderer.render(this.scene, this.camera);
+  };
+  // MODEL ROTATION CHANGE VIA ROTATION INPUT
+  objRotation = (direction, axis) => {
+    this.gltf[direction][axis] = this.state[direction][axis];
+    this.renderer.render(this.scene, this.camera);
+  };
+  // MODEL SCALE CHAGE VIA SCALE INPUT
+  objScale = (direction, axis) => {
+    this.gltf[direction][axis] = this.state[direction][axis];
+    this.renderer.render(this.scene, this.camera);
   };
 
   render() {
     return (
       <div className="inputDiv">
         <div
-          style={{width: "400px", height: "400px"}}
+          style={{width: "600px", height: "600px"}}
           ref={mount => this.mount = mount}>
         </div>
 
         <div>
-          <p>Camera</p>
-          <input type="number" value={this.state.cameraPosition.x}
-                  onChange={event => this.stateUpdate(event, "cameraPosition", "x", this.cameraMove)}/>
-          <input type="number" value={this.state.cameraPosition.y}
-                  onChange={event => this.stateUpdate(event, "cameraPosition", "y", this.cameraMove)}/>
-          <input type="number" value={this.state.cameraPosition.z}
-                  onChange={event => this.stateUpdate(event, "cameraPosition", "z", this.cameraMove)}/>
-        </div>
-
-        <div>
-        <div>
-          <p>Position</p>
-          <input type="number" value={this.state.position.x}
-                  onChange={event => this.stateUpdate(event, "position", "x", this.objPosition)}/>
-          <input type="number" value={this.state.position.y}
-                  onChange={event => this.stateUpdate(event, "position", "y", this.objPosition)}/>
-          <input type="number" value={this.state.position.z}
-                  onChange={event => this.stateUpdate(event, "position", "z", this.objPosition)}/>
-        </div>
+          <div>
+            <p>Position</p>
+            <input type="number" value={this.state.position.x}
+                    onChange={event => this.stateUpdate(event, "position", "x", this.objPosition)}/>
+            <input type="number" value={this.state.position.y}
+                    onChange={event => this.stateUpdate(event, "position", "y", this.objPosition)}/>
+            <input type="number" value={this.state.position.z}
+                    onChange={event => this.stateUpdate(event, "position", "z", this.objPosition)}/>
+          </div>
         </div>
 
         <div>
@@ -144,9 +174,9 @@ class App extends React.Component {
           <p>Rotation</p>
           <input type="number" value={this.state.rotation.x}
                   onChange={event => this.stateUpdate(event, "rotation", "x", this.objRotation)}/>
-          <input type="number" value={this.state.rotation.y}
+                <input type="number" value={this.state.rotation.y}
                   onChange={event => this.stateUpdate(event, "rotation", "y", this.objRotation)}/>
-          <input type="number" value={this.state.rotation.z}
+                <input type="number" value={this.state.rotation.z}
                   onChange={event => this.stateUpdate(event, "rotation", "z", this.objRotation)}/>
         </div>
         </div>
@@ -165,18 +195,10 @@ class App extends React.Component {
 
         <p>Hold Left Click Down to movie the object</p>
         <div>
-          <select ref={valType => this.valType = valType}>
-            <option value="cameraPosition">camera</option>
+          <select onChange={this.posRotScale} ref={valType => this.valType = valType}>
             <option value="position">position</option>
             <option value="rotation">rotation</option>
             <option value="scale">scale</option>
-          </select>
-        </div>
-        <div>
-          <select ref={valueAxis => this.valueAxis = valueAxis}>
-            <option value="x">x</option>
-            <option value="y">y</option>
-            <option value="z">z</option>
           </select>
         </div>
 
